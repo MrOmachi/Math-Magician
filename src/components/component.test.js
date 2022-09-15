c0fec62dadcd4733a3a2970c8cc42fc46f199d9d
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-14 19:27:28 -0500

    Add logic jest test

--- src/components/component.test.js
+++ src/components/component.test.js
@@ -1,37 +1,37 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter as Router } from "react-router-dom";
-import Home from "./pages/Home";
-import Quotes from "./pages/Quotes";
-import Calculator from "./Calculator";
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import Home from './pages/Home';
+import Quotes from './pages/Quotes';
+import Calculator from './Calculator';
 
-describe("Testing the App UI", () => {
-  test("Testing home UI component", () => {
+describe('Testing the App UI', () => {
+  test('Testing home UI component', () => {
     render(
       <Router>
         <Home />
-      </Router>
+      </Router>,
     );
   });
 
-  test("Calculator UI component", async () => {
+  test('Calculator UI component', async () => {
     render(
       <Router>
         <Calculator />
-      </Router>
+      </Router>,
     );
-    const btn = await screen.findAllByRole("button");
+    const btn = await screen.findAllByRole('button');
     expect(btn.length).toBe(19);
   });
 
-  test("Quote UI component", () => {
+  test('Quote UI component', () => {
     render(
       <Router>
         <Quotes />
-      </Router>
+      </Router>,
     );
 
     const str = screen.getByText(
-      /Mathematics is not about numbers, equations, computations or algorithms, it is about understanding - William Paul Thurston/i
+      /Mathematics is not about numbers, equations, computations or algorithms, it is about understanding - William Paul Thurston/i,
     );
 
     expect(str).toBeInTheDocument();
--- src/components/logic/logic.test.js
+++ src/components/logic/logic.test.js
@@ -1,1 +1,22 @@
+import calculate from './calculator';
+import operate from './operate';
 
+describe('Testing functional components', () => {
+  test('Testing calculate function', () => {
+    const state = {
+      total: 0,
+      next: 3,
+      operation: null,
+    };
+    const data = {
+      total: 3,
+      next: null,
+      operation: '+',
+    };
+
+    expect(calculate(state, '+')).toEqual(data);
+  });
+  test('Testing operate function', () => {
+    expect(operate(10, 5, '-')).toBe('5');
+  });
+});
